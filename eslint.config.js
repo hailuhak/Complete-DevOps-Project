f202e44902ba65b4765dc9d91c2712da5928e4a8
hailu <hailu@gmail.com>
2026-09-11 02:14:25 +0300

    Configure ESLint and Prettier for backend

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  {
+    files: ["**/*.js"],
+    ignores: ["node_modules/**"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+];
